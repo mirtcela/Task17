1ba1184e01af647d742f0ae7067b8314d792320d
mirtcela <mirtcela@yandex.ru>
2020-09-08 22:55:41 +0300

    -изменение условия остановки

--- src/Task17.java
+++ src/Task17.java
@@ -3,20 +3,23 @@ import java.util.Scanner;
 import java.io.FileNotFoundException;
 
 public class Task17 {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args){
         try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
             String str;
             System.out.println("Признак конца ввода строки - слово 'стоп'");
             try (FileWriter fw = new FileWriter("C:\\test\\writetest.txt")) {
                 do {
+                    System.out.print(":");
                     str = br.readLine();
-                    str = str+"\r\n";
-                    fw.write(str);}
-                while (!(str = br.readLine()).equals("стоп")) ;
+                    if (str.compareTo("стоп") == 0) break;
+                    str = str + "\r\n";
+                    fw.write(str);
+                }
+                while (str.compareTo("стоп") != 0);
             }
         }
         catch (IOException e) {
-            e.printStackTrace();
+            System.out.println("Ошибка ввода-вывода");
         }
     }
 }
