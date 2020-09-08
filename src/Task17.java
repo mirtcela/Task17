03b9808816d6b4e00082e12de8b55d767494f197
mirtcela <mirtcela@yandex.ru>
2020-09-08 20:52:54 +0300

    -добавление пути при создании файла

--- src/Task17.java
+++ src/Task17.java
@@ -1,16 +1,17 @@
 import java.io.*;
 import java.util.Scanner;
+import java.io.FileNotFoundException;
 
 public class Task17 {
-    public static void main(String[] args){
+    public static void main(String[] args) throws IOException {
         try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
             String str;
             System.out.println("Признак конца ввода строки - слово 'стоп'");
-            try (BufferedWriter bw = new BufferedWriter(new FileWriter("writetext.txt"))) {
+            try (FileWriter fw = new FileWriter("C:\\test\\writetest.txt")) {
                 do {
                     str = br.readLine();
-                    bw.write(str);}
-                    while (!(str = br.readLine()).equals("стоп")) ;
+                    fw.write(str);}
+                while (!(str = br.readLine()).equals("стоп")) ;
             }
         }
         catch (IOException e) {
